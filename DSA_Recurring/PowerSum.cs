a7dd41a69ab7e3b164d4752dc2b0547a23b933fc
Chit San Maung (CW) <chit.san.maung@wdc.com>
2025-07-31 11:04:27 +0700

    day 4,5  , Recurrsion updated

---
+++ DSA_Recurring/PowerSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList_DSA_Training.DSA_Recurring
+{
+    public class PowerSum
+    {
+        public static double powerSumRec(Object[] array , int power =1)
+        {
+
+            double sum = 0;
+
+            foreach(var item in array)
+            {
+                if(item is object[] nestedArray)
+                {
+                    sum += powerSumRec(nestedArray, power + 1); 
+                }
+
+                else if (item is int number)
+                {
+                    sum += number; 
+                }
+            }
+
+            return Math.Pow(sum, power);
+
+        }
+    }
+}
